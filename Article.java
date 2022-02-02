7309c904dbc6fcd53443788d8b949d036198c4e0
blewis1014 <70607011+blewis1014@users.noreply.github.com>
2022-02-02 11:45:40 -0500

    Add files via upload

---
+++ Article.java
@@ -0,0 +1,198 @@
+package edu.odu.cs411yellow.gameeyebackend.mainbackend.models.resources;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Represents a news article resource.
+ */
+public class Article {
+    private String id;
+
+    /**
+     * Title of the article.
+     */
+    private String title;
+
+    /**
+     * URL to the page containing the article.
+     */
+    private String url;
+
+    /**
+     * Name of the news website from which this article was retrieved.
+     */
+    private String newsWebsiteName;
+
+    /**
+     * Reference to a thumbnail for this article, if any.
+     */
+    private String thumbnailId;
+
+    /**
+     * Small portion of the text body of an article, usually the first few sentences.
+     * Limit to 255 characters.
+     */
+    private String snippet;
+
+    /**
+     * Date the article was published.
+     */
+    private Date publicationDate;
+
+    /**
+     * Date the document was last updated.
+     */
+    private Date lastUpdated;
+
+    /**
+     * Denotes whether or not an article is important, computed with machine learning.
+     */
+    private boolean isImportant;
+
+    @PersistenceConstructor
+    public Article(String id, String title, String url, String newsWebsiteName, String thumbnailId, String snippet,
+                   Date publicationDate, Date lastUpdated, boolean isImportant) {
+
+        if (id == null || id.equals("")) {
+            this.id = ObjectId.get().toHexString();
+        }
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.newsWebsiteName = newsWebsiteName;
+        this.thumbnailId = thumbnailId;
+        this.snippet = snippet;
+        this.publicationDate = publicationDate;
+        this.lastUpdated = lastUpdated;
+        this.isImportant = isImportant;
+    }
+
+    public Article() {
+        this.id = ObjectId.get().toHexString();
+        this.title = "";
+        this.url = "";
+        this.newsWebsiteName = "";
+        this.thumbnailId = "";
+        this.snippet = "";
+        this.publicationDate = new Date();
+        this.lastUpdated = new Date();
+        this.isImportant = false;
+    }
+
+    public Article(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.url = article.getUrl();
+        this.newsWebsiteName = article.getNewsWebsiteName();
+        this.thumbnailId = article.getThumbnailId();
+        this.snippet = article.getSnippet();
+        this.publicationDate = article.getPublicationDate();
+        this.lastUpdated = article.getLastUpdated();
+        this.isImportant = article.getIsImportant();
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getNewsWebsiteName() {
+        return this.newsWebsiteName;
+    }
+
+    public void setNewsWebsiteName(String newsWebsiteName) {
+        this.newsWebsiteName = newsWebsiteName;
+    }
+
+    public String getThumbnailId() {
+        return this.thumbnailId;
+    }
+
+    public void setThumbnailId(String thumbnailId) {
+        this.thumbnailId = thumbnailId;
+    }
+
+    public String getSnippet() {
+        return this.snippet;
+    }
+
+    public void setSnippet(String snippet) {
+        this.snippet = snippet;
+    }
+
+    public Date getPublicationDate() {
+        return this.publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Date getLastUpdated() {
+        return this.lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public boolean getIsImportant() {
+        return this.isImportant;
+    }
+
+    public void setIsImportant(boolean isImportant) {
+        this.isImportant = isImportant;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o ) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article that = (Article) o;
+
+        return Objects.equals(title, that.title)
+                && Objects.equals(newsWebsiteName, that.newsWebsiteName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, url, newsWebsiteName, thumbnailId, snippet, publicationDate, isImportant);
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper obj= new ObjectMapper();
+        String result;
+
+        try {
+            result = obj.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            result = "JsonProcessingException";
+        }
+
+        return result;
+    }
+}
---
+++ UniversalScraper.java
@@ -0,0 +1,130 @@
+package edu.odu.cs411yellow.gameeyebackend.mainbackend.webscraping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.NewsWebsite;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.resources.Article;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.repositories.NewsWebsiteRepository;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UniversalScraper implements WebScraper {
+
+    NewsWebsiteRepository newsWebsites;
+
+    @Autowired
+    public UniversalScraper(NewsWebsiteRepository newsWebsites) {
+        this.newsWebsites = newsWebsites;
+    }
+
+    /**
+     * Initiate the scrape
+     */
+    @Override
+    public List<Article> scrape(String newsOutlet) {
+        List<Article> articles = new ArrayList<>();
+
+        try {
+            NewsWebsite newsSite = newsWebsites.findByName(newsOutlet);
+
+            String url = newsSite.getRssFeedUrl();
+            Document rssFeed = Jsoup.connect(url).get();
+
+            Elements items = rssFeed.select("item");
+
+            for (var i : items) {
+                Article toAdd = createArticle(i, newsSite.getName());
+                articles.add(toAdd);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return articles;
+    }
+
+    @Override
+    public Article createArticle(Element i, String websiteName) throws ParseException {
+        DateFormat format = new SimpleDateFormat("E, d MMMM yyyy kk:mm:ss z");
+        String title = i.select("title").text();
+
+        String url = i.select("link").text();
+
+        String snippet;
+
+        //parse date
+        String pubDate = i.select("pubDate").text();
+        Date publicationDate;
+        if (!pubDate.isEmpty()) {
+            publicationDate = format.parse(pubDate);
+        } else {
+            publicationDate = new Date();
+        }
+
+        //parse snippet
+        if (websiteName.contentEquals("IGN")) {
+            snippet = i.select("description").text();
+        } else if (websiteName.contentEquals("PC Gamer")) {
+            Document body = Jsoup.parse(i.selectFirst("title").nextElementSibling().text());
+            Elements paragraph = body.select("p");
+            snippet = paragraph.text();
+        } else {
+            Document body = Jsoup.parse(i.select("description").text());
+            Elements paragraph = body.select("p");
+            snippet = paragraph.text();
+        }
+
+        if (snippet.length() > 255) {
+            snippet = snippet.substring(0, 255);
+        }
+
+        Article article = new Article();
+        article.setTitle(title);
+        article.setUrl(url);
+        article.setNewsWebsiteName(websiteName);
+        article.setSnippet(snippet);
+        article.setPublicationDate(publicationDate);
+        article.setLastUpdated(publicationDate);
+        article.setIsImportant(false);
+
+        return article;
+    }
+
+
+    /**
+     * Output to JSON format
+     *
+     * @return JSON
+     */
+    //@Override
+    public String toString(String name) {
+        ObjectMapper obj = new ObjectMapper();
+        String articlesStr = "";
+        List<Article> articles = scrape(name);
+
+        for (Article a : articles) {
+            try {
+                String temp;
+                temp = obj.writerWithDefaultPrettyPrinter().writeValueAsString(a);
+                articlesStr = String.format("%1$s\n%2$s", articlesStr, temp);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return articlesStr;
+    }
+}
